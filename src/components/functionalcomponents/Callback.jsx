f3127d571027bef3626367719ba2c2e0a5a1615d
subapriya-j <ABC>
2024-01-28 16:03:53 +0530

    SUBA

--- src/components/functionalcomponents/Callback.jsx
+++ src/components/functionalcomponents/Callback.jsx
@@ -4,7 +4,7 @@ import React, { useState} from "react";
 const Callback = () => {
     const [count, setCount] = useState(0);
     const list=[10,20,30,40,50]
-    const list1=[...list,60,70,100,200,300]
+    const list1=[...list,60,70]
     list.forEach(element => {
         console.log(element)
     });
@@ -14,12 +14,12 @@ const Callback = () => {
             <h1>This is Call Back Example</h1>
             <h2>Count: {count}</h2>
             <button onClick={() => setCount(count + 1)}>Add one</button><br></br>
-            <ol>
+            <ul>
                 
                 {list1.map((val,index)=>(
                     <li key={index}>{val}</li>
                 ))}
-            </ol>
+            </ul>
         </div>
     );
 }
